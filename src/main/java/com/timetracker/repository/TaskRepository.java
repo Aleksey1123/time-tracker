91050fb48212ec57fe09405cbff544f00d3cef8a
Aleksey1123 <aleksey.rytov@gmail.com>
2024-02-20 00:25:08 +0300

    edited TaskRepository

--- src/main/java/com/timetracker/repository/TaskRepository.java
+++ src/main/java/com/timetracker/repository/TaskRepository.java
@@ -1,7 +1,12 @@
 package com.timetracker.repository;
 
+import com.timetracker.entity.Employee;
 import com.timetracker.entity.Task;
+import org.springframework.data.domain.Page;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Integer> {
+
 }
