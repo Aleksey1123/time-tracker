b9de03758a8bee50b0cb83937e1156a577272b67
Aleksey1123 <aleksey.rytov@gmail.com>
2024-02-20 01:37:27 +0300

    edited PageBuilder for better validation

--- src/main/java/com/timetracker/sorting/PageBuilder.java
+++ src/main/java/com/timetracker/sorting/PageBuilder.java
@@ -13,8 +13,9 @@ public class PageBuilder {
     private static final int DEFAULT_PAGE = 0;
     private static final int DEFAULT_PAGE_SIZE = 10;
 
-    private final Set<String> FIELDS = Set.of("id", "firstName", "lastName",
-            "name", "status");
+    private final Set<String> EMPLOYEE_FIELDS = Set.of("id", "firstName", "lastName");,
+    private final Set<String> TASK_FIELDS = Set.of("id", "name");
+    private final Set<String> HISTORY_FIELDS = Set.of("id", "status");
     private final Set<String> ENTITIES = Set.of(EntityNames.Employee.name(),
             EntityNames.Task.name(),
             EntityNames.History.name());
@@ -24,10 +25,13 @@ public class PageBuilder {
         int queryPageNumber = pageNumber;
         int queryPageSize = pageSize;
 
-        if (ENTITIES.contains(entityName))
-            if (!FIELDS.contains(sortingParam))
+        if (ENTITIES.contains(entityName)) {
+            if ((entityName.equals(EntityNames.Employee.name()) && !EMPLOYEE_FIELDS.contains(sortingParam)) ||
+                    (entityName.equals(EntityNames.Task.name()) && !TASK_FIELDS.contains(sortingParam)) ||
+                    (entityName.equals(EntityNames.History.name()) && !HISTORY_FIELDS.contains(sortingParam)))
                 throw new IllegalStateException("Unexpected value: " + sortingParam +
                         "for class " + entityName);
+        }
 
         if (pageNumber > 0) {
             queryPageNumber -= 1;
