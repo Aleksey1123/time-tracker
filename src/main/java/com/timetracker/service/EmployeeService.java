392397feebc7e71d5d55a59286b00f8015aaf4b3
Aleksey1123 <aleksey.rytov@gmail.com>
2024-05-10 16:59:22 +0300

    adding getAllEmployees method

--- src/main/java/com/timetracker/service/EmployeeService.java
+++ src/main/java/com/timetracker/service/EmployeeService.java
@@ -1,11 +1,13 @@
 package com.timetracker.service;
 
 import com.timetracker.entity.Employee;
-import com.timetracker.entity.Task;
 import com.timetracker.model.EmployeeDTO;
-import com.timetracker.model.TaskDTO;
+import org.springframework.data.domain.Page;
 
 public interface EmployeeService {
+
+    Page<EmployeeDTO> getAllEmployees(Integer pageNumber, Integer pageSize,
+                                      String sortingParam);
 
     EmployeeDTO getEmployeeById(Integer id);
 
--- src/main/java/com/timetracker/service/EmployeeServiceImpl.java
+++ src/main/java/com/timetracker/service/EmployeeServiceImpl.java
@@ -1,21 +1,22 @@
 package com.timetracker.service;
 
 import com.timetracker.entity.Employee;
-import com.timetracker.entity.Task;
-import com.timetracker.enums.Status;
+import com.timetracker.enums.EntityNames;
 import com.timetracker.exception.EmployeeAlreadyExistsException;
 import com.timetracker.exception.EmployeeNotFoundException;
-import com.timetracker.exception.TaskAlreadyExistsException;
 import com.timetracker.exception.TaskNotFoundException;
 import com.timetracker.mapper.EmployeeMapper;
 import com.timetracker.model.EmployeeDTO;
 import com.timetracker.repository.EmployeeRepository;
 import com.timetracker.sorting.PageBuilder;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
-import java.time.LocalDateTime;
 import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -26,6 +27,18 @@ public class EmployeeServiceImpl implements EmployeeService {
     private final EmployeeMapper employeeMapper = EmployeeMapper.INSTANCE;
     private static final String EMPLOYEE_NOT_FOUND_EXC_MESSAGE = "employee not found";
     private static final String EMPLOYEE_ALREADY_EXISTS_EXC_MESSAGE = "employee already exists, provide a different name";
+
+    @Override
+    public Page<EmployeeDTO> getAllEmployees(Integer pageNumber, Integer pageSize,
+                                     String sortingParam) {
+        PageRequest pageRequest = pageBuilder.buildPageRequest(pageNumber, pageSize,
+                sortingParam, EntityNames.Employee.name());
+
+        return new PageImpl<>(employeeRepository.findAll(pageRequest)
+                .stream()
+                .map(employeeMapper::employeeToEmployeeDTO)
+                .collect(Collectors.toList()));
+    }
 
     @Override
     public EmployeeDTO getEmployeeById(Integer id) {
